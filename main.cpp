44d4f4737136e7298b2c92850b4d2bfe8486ae95
Cauatop <pedrocaua322@gmail.com>
2026-03-18 19:27:32 -0300

    #2 - issue completa

--- main.cpp
+++ main.cpp
@@ -2,14 +2,32 @@
 using namespace std;
 
 int main() {
-    //Numeros N > 0
     int N;
     cout << "Digite um numero inteiro positivo: ";
-    cin >> N;
-    //Verificar se o numero é positivo
-    if (N <= 0) {
-        cout << "Numero invalido. Digite um numero inteiro positivo." << endl;
-        return 1; //Encerrar o programa com código de erro
+    if (!(cin >> N) || N <= 0) {
+        cout << "Digite um numero inteiro maior que 0." << endl;
+        return 1;
     }
-    
+    cout << "Valor de N: " << N << endl;
+    cout << "Numeros primos: ";
+    int count = 0;
+    bool primeiro = true;
+    for (int i = 2; i <= N; i++) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            if (!primeiro) cout << " - ";
+            cout << i;
+            primeiro = false;
+            count++;
+        }
+    }
+    cout << endl;
+    cout << "Quantidade de numeros primos: " << count << endl;
+    return 0;
 }
